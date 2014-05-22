919097ff861611ae0077f8ec4840990946242a22
Denis Ryzhkov <denisr@denisr.com>
2014-05-22 09:42:40 +0300

    v0.1.5: Improved config, docs, tests.

--- bn.py
+++ bn.py
@@ -6,7 +6,7 @@ Lightweight profiling tool to detect performance BottleNecks in Python code.
 Please see full description here:
 https://github.com/denis-ryzhkov/bn/blob/master/README.md
 
-bn version 0.1.4
+bn version 0.1.5
 Copyright (C) 2013 by Denis Ryzhkov <denisr@denisr.com>
 MIT License, see http://opensource.org/licenses/MIT
 '''
@@ -20,12 +20,13 @@ from time import time
 
 class Bn(object):
 
-    def __init__(self):
+    def __init__(self, total_key='TOTAL', format='{key}={seconds:.4f}', sep=', '):
         self.stats = defaultdict(int)
         self.time = 0
         self.key = None
-        self.total_key = 'TOTAL'
-        self.format = '{seconds:>10.4f}    {key}'
+        self.total_key = total_key
+        self.format = format
+        self.sep = sep
 
     def __call__(self, key=None):
         now = time()
@@ -38,11 +39,12 @@ class Bn(object):
 
     @property
     def total(self):
+        self() # Save last seconds.
         return self.stats[self.total_key]
 
     def __str__(self):
         self() # Save last seconds.
-        return '\n'.join(
+        return self.sep.join(
             self.format.format(seconds=seconds, key=key)
             for key, seconds in
             sorted(self.stats.iteritems(), key=lambda (key, seconds): -seconds)
@@ -50,6 +52,8 @@ class Bn(object):
 
     def reset(self):
         self.__init__()
+
+#### global bn
 
 bn = Bn()
 
@@ -59,42 +63,28 @@ def test():
 
     import re
     from time import sleep
+    bn = Bn()
 
-    bn('iteration')
-
+    bn('loop')
     for item in xrange(10):
 
-        bn('step 1')
+        bn('rabbit')
         sleep(0.1)
 
-        bn('step 2')
+        bn('turtle')
         sleep(0.2)
 
-        bn('iteration')
-
-    bn() # Stop tracking.
+        bn('loop')
 
     if bn.total > 1.0:
-        # logging.info(bn)
-        print(bn)
+        print(bn) # logging.info(bn)
+        # Microseconds may differ:
+        # TOTAL=3.0053, turtle=2.0030, rabbit=1.0019, loop=0.0004
 
-    '''
-    3.0053    TOTAL
-    2.0030    step 2
-    1.0019    step 1
-    0.0004    iteration
-    '''
-
-    assert 2 < bn.total < 4
-
+    # But seconds are OK to test.
+    assert 2 < bn.total < 4, bn.total
     result = re.sub(r'\.\d{4}', '.0123', str(bn))
-
-    assert result.strip() == '''
-    3.0123    TOTAL
-    2.0123    step 2
-    1.0123    step 1
-    0.0123    iteration
-    '''.strip()
+    assert result == 'TOTAL=3.0123, turtle=2.0123, rabbit=1.0123, loop=0.0123', result
 
     print('OK')
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='bn',
-    version='0.1.4',
+    version='0.1.5',
     description='Lightweight profiling tool to detect performance BottleNecks in Python code.',
     long_description='''
 Hence the name: bn - BottleNeck.  
@@ -10,47 +10,51 @@ And you know keys B and N are very comfortable to type quickly in a row.
 
 Usage::
 
-    bn('iteration')
-
+    bn('loop')
     for item in generator:
 
-        bn('step 1')
-        # code of step 1
+        bn('rabbit')
+        # code of rabbit
 
-        bn('step 2')
-        # code of step 2
+        bn('turtle')
+        # code of turtle
 
-        bn('iteration')
-
-    bn() # Stop tracking.
+        bn('loop')
 
     if bn.total > 1.0:
         logging.info(bn)
 
 Result::
 
-    22.2632    TOTAL
-    20.6403    step 2
-     1.6209    step 1
-     0.0020    iteration
-
-Scope::
-
-    pip install bn
-    from bn import bn
+    TOTAL=22.2632, turtle=20.6403, rabbit=1.6209, loop=0.0020
+    #
+    # This config is default in bn>=0.1.5:
+    # Bn(total_key='TOTAL', format='{key}={seconds:.4f}', sep=', ')
 
     # OR:
 
+    22.2632    TOTAL
+    20.6403    turtle
+     1.6209    rabbit
+     0.0020    loop
+    #
+    # This config is default in bn<=0.1.4:
+    # Bn(total_key='TOTAL', format='{seconds:>10.4f}    {key}', sep='\n')
+
+Install::
+
+    pip install bn
+
+Scope::
+
+    # Global "bn" is useful to profile cross-module without passing "bn" explicitly.
+    from bn import bn
+    bn.format = custom_format
+
+    # Scoped "Bn" is useful to have multiple independent profilers.
     from bn import Bn
     def action():
-        bn = Bn()
-
-Config::
-
-    bn.total_key = 'TOTAL'
-    bn.format = '{seconds:>10.4f}    {key}'
-
-See also: `bn.py:test() <https://github.com/denis-ryzhkov/bn/blob/master/bn.py#L58>`_.
+        bn = Bn(format=custom_format)
 
 ''',
     url='https://github.com/denis-ryzhkov/bn',
